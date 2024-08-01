94022e5e6dc2ead4448c4dd4f6a2a2b0dd8c605c
Eddie <zhizhang.teng@shopee.com>
2024-08-01 14:53:38 +0800

    use maven replace gradle

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Component
+    public static class MyComponent implements CommandLineRunner {
+
+        @Value("${spring.application.name}")
+        private String applicationName;
+
+        @Override
+        public void run(String... args) {
+            System.out.println("Hello, " + applicationName + "!");
+        }
+    }
 
 }
